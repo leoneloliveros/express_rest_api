c0f04a4b8b7895e0f25390707340bea1b46dc2e0
Leonel Oliveros <leoneloliveros.co@gmail.com>
2022-01-18 15:31:19 -0500

    Add error handler middleware for 404 routes

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ import express from 'express';
 // Import routes
 import placesRoutes from './routes/places.routes.js';
 
+// Utils
+import HttpError from './models/http-error.model.js';
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.use('/api/places', placesRoutes); // => /api/places...
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this routes.', 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
